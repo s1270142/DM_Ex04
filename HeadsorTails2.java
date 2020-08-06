a97dfff2f772e01da083e776fc4eae6ef841991a
s1270142 <69298737+s1270142@users.noreply.github.com>
2020-08-06 23:26:00 +0900

    Add files via upload

---
+++ HeadsorTails2.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HeadsorTails2{
+
+	public static void main(String[] args) {
+		Random rnd = new Random();
+		int head=0;
+        int tail=0;
+        System.out.println("Who are you?");
+		System.out.print(">");
+		Scanner scan=new Scanner(System.in);
+		String name=scan.next();
+		System.out.println("Hello, "+name+"!");
+		System.out.println("Tossing a coin...");
+		for(int i=1;i<=3;i++) {
+			int x=rnd.nextInt(2);
+			if(x==0) {
+				System.out.println("Round "+i+": Heads");
+				head++;
+			}
+			else {
+				System.out.println("Round "+i+": Tails");
+				tail++;
+			}
+		}
+        System.out.println("Heads:"+head+","+"Tails:"+tail);
+        if(head>tail){
+			System.out.println(name+" won");
+		}
+		else System.out.println(name+" lost");
+	}
+
+}
