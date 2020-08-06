37fe978f293446ed3257e593d66ae00d8598b180
s1270142 <s1270142@u-aizu.ac.jp>
2020-08-06 23:15:04 +0900

    User name added

--- HeadsorTails2.java
+++ HeadsorTails2.java
@@ -1,11 +1,17 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class HeadsorTails2{
 
 	public static void main(String[] args) {
 		Random rnd = new Random();
 		int head=0;
-		int tail=0;
+        int tail=0;
+        System.out.println("Who are you?");
+		System.out.print(">");
+		Scanner scan=new Scanner(System.in);
+		String name=scan.next();
+		System.out.println("Hello, "+name+"!");
 		System.out.println("Tossing a coin...");
 		for(int i=1;i<=3;i++) {
 			int x=rnd.nextInt(2);
